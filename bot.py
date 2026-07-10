bd03652588bcb679c5f57e21c1fae66d8123942c
krishansharma08092000-prog <krishansharma08092000@gmail.com>
2026-07-10 19:11:20 +0530

    Initialize Telegram bot with start command

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+# bot.py
+import telebot
+import os
 
+BOT_TOKEN = "8912269941:AAGkKwNTZOX4nbiuVl5eE9LOtoths3Oqb4c"
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "✅ Render पर बोट चल रहा है!")
+
+print("🤖 BOT STARTING ON RENDER...")
+bot.polling(none_stop=True)
